beef4207f7a5361fe85f90ae33ded0eeba7087e1
Mexus209 <spikemorow@gmail.com>
2021-09-27 23:15:56 +0300

    lesson 3 digitNumber fixed

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -3,6 +3,7 @@
 package lesson3.task1
 
 import lesson1.task1.sqr
+import kotlin.math.abs
 import kotlin.math.sqrt
 
 // Урок 3: циклы
@@ -76,7 +77,7 @@ fun digitCountInNumber(n: Int, m: Int): Int =
 fun digitNumber(n: Int): Int {
     var count = 0
     var number = n
-    if (number == 0) return 1
+    if (abs(number) == 0) return 1
     while (number > 0) {
         count++
         number /= 10
